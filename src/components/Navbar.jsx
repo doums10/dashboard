0b800e9d69e5b4b0a6a77fb3f6d08a0aba8f6e00
Mamadou Sylla <m27sylla@gmail.com>
2022-09-07 11:51:44 -0400

    navbar done, context to add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,18 @@ import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 	<TooltipComponent content={title} position="BottomCenter">
-		<button type="button"
-		onClick={customFunc}
-		style={{color}}
-		className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-		<span style={{background:dotColor}}
-		className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-		{icon}
-		</span>
-
+		<button
+			type="button"
+			onClick={customFunc}
+			style={{ color }}
+			className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+		>
+			<span
+				style={{ background: dotColor }}
+				className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+			>
+				{icon}
+			</span>
 		</button>
 	</TooltipComponent>
 );
@@ -32,9 +35,45 @@ const Navbar = () => {
 				title="Menu"
 				customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
 				color="blue"
-				icon={<AiOutlineMenu/>}
-
+				icon={<AiOutlineMenu />}
 			/>
+			<div className="flex">
+				<NavButton
+					title="Cart"
+					customFunc={() => handleClick("cart")}
+					color="blue"
+					icon={<FiShoppingCart />}
+				/>
+				<NavButton
+					title="Chat"
+					customFunc={() => handleClick("chat")}
+					dotColor="#03C9D7"
+					color="blue"
+					icon={<BsChatLeft />}
+				/>
+				<NavButton
+					title="Notifications"
+					customFunc={() => handleClick("notification")}
+					dotColor="#03C9D7"
+					color="blue"
+					icon={<RiNotification3Line />}
+				/>
+				<TooltipComponent content="Profile" position="BottomCenter">
+					<div
+						className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+						onClick={() => handleClick("userProfile")}
+					>
+						<img src={avatar} alt="" className="rounded-full w-8 h-8" />
+						<p>
+							<span className="text-gray-400 text-14">Hi, </span>
+							<span className="text-gray-400 font-bold ml-1 text-14">
+								Michael
+							</span>
+						</p>
+						<MdKeyboardArrowDown className="text-gray-400 text-14" />
+					</div>
+				</TooltipComponent>
+			</div>
 		</div>
 	);
 };
