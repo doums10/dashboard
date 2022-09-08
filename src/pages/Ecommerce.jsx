db2acef67541a156b0eb4e909760e43a11478573
Mamadou Sylla <m27sylla@gmail.com>
2022-09-08 15:28:22 -0400

    all imports sone

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+
+
 
 const Ecommerce = () => {
 		return (
