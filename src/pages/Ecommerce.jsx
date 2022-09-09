c55d9a131370767e96c2773cebb4aedb602b08e3
Mamadou Sylla <m27sylla@gmail.com>
2022-09-09 09:03:55 -0400

    button download done

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -16,9 +16,18 @@ const Ecommerce = () => {
 				<div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
 				<div className="flex justify-between items-center">
 					<div>
-						<p>Earnings</p>
-						<p>$63,448.78</p>
+						<p className="font-bold text-gray-400">Earnings</p>
+						<p className="text-2xl">$63,448.78</p>
 					</div>
+				</div>
+				<div className="mt-6">
+					<Button 
+						color="white"
+						bgColor="blue"
+						text="Download"
+						borderRadius="10px"
+						size="md"
+					/>
 				</div>
 				</div>
 			</div>
