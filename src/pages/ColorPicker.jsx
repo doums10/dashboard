9b41e3f71b97ed801619f54be853a5d48d688fed
Mamadou Sylla <m27sylla@gmail.com>
2022-09-20 16:18:06 -0400

    function added to change color on pen preview

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,7 +3,10 @@ import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 import { Header } from "../components";
 
 // function to change color on pen preview
-
+const change = (args) => {
+	document.getElementById("preview").style.backgroundColor =
+		args.currentValue.hex;
+};
 const ColorPicker = () => {
 	return (
 		<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -20,6 +23,7 @@ const ColorPicker = () => {
 							modeSwitcher={false}
 							inline
 							showButtons={false}
+							change={change}
 						/>
 					</div>
 					<div>
@@ -30,6 +34,7 @@ const ColorPicker = () => {
 							modeSwitcher={false}
 							inline
 							showButtons={false}
+							change={change}
 						/>
 					</div>
 				</div>
