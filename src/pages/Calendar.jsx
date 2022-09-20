77d5fa70137d00f3037cee8ef3b31d582d0863dc
Mamadou Sylla <m27sylla@gmail.com>
2022-09-20 10:21:37 -0400

    kanban structure component done with all imports

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop  } from '@syncfusion/ej2-react-schedule'
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop  } from '@syncfusion/ej2-react-schedule'
 import { scheduleData } from '../data/dummy'
 import Header from '../components/Header';
 
