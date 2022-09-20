3479d287872f84593fd4034cbd503f1804e292db
Mamadou Sylla <m27sylla@gmail.com>
2022-09-20 16:11:34 -0400

    inline palette and inline picker added

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
-import { Header } from '../components';
+import { Header } from "../components";
+
+// function to change color on pen preview
 
 const ColorPicker = () => {
-		return (
-				<div>ColorPicker</div>
-		)
-}
+	return (
+		<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+			<Header category="App" title="Color-Picker" />
+			<div className="text-center">
+				<div id="preview" />
 
-export default ColorPicker
+				<div className="flex justify-center items-center gap-20 flex-wrap">
+					<div>
+						<p className="text-2xl font-semibold mt-2 mb-4">Inline Palette</p>
+						<ColorPickerComponent
+							id="inline-palette"
+							mode="Palette"
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+						/>
+					</div>
+					<div>
+						<p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+						<ColorPickerComponent
+							id="inline-picker"
+							mode="Picker"
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ColorPicker;
