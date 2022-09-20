c47e87bda23296eb8317c10b109b61c9f903aa21
Mamadou Sylla <m27sylla@gmail.com>
2022-09-20 16:29:13 -0400

    themesettings component structure done

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const App = () => {
 						</div>
 
 						<div>
+						<ThemeSettings/>
 							<Routes>
 								{/* Dashboard */}
 								<Route path="/" element={<Ecommerce />} />
--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 const ThemeSettings = () => {
-		return (
-				<div>ThemeSettings</div>
-		)
-}
+	return <div className="bg-half-transparent">ThemeSettings</div>;
+};
 
-export default ThemeSettings
+export default ThemeSettings;
