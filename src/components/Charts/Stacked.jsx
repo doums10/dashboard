5a5e7a373355336bbcab3140337e3747d66aea3c
Mamadou Sylla <m27sylla@gmail.com>
2022-09-23 15:39:30 -0400

    button exit to do on cart view

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+import React from "react";
+import {
+	ChartComponent,
+	SeriesCollectionDirective,
+	SeriesDirective,
+	Inject,
+	Legend,
+	Category,
+	StackingColumnSeries,
+	Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import {
+	stackedCustomSeries,
+	stackedPrimaryXAxis,
+	stackedPrimaryYAxis,
+} from "../../data/dummy";
 
-const Stacked = ({width, height, }) => {
-		return (
-			<ChartComponent
+const Stacked = ({ width, height }) => {
+	return (
+		<ChartComponent
 			width={width}
 			height={height}
 			id="charts"
 			primaryXAxis={stackedPrimaryXAxis}
 			primaryYAxis={stackedPrimaryYAxis}
-			chartArea={{ border: { width: 0}}}
+			chartArea={{ border: { width: 0 } }}
 			tooltip={{ enable: true }}
-			legendSettings={{ background:"white"}}
-			>
-			<Inject services={[Legend, Category, StackingColumnSeries, Tooltip, ]}/>
+			legendSettings={{ background: "white" }}
+		>
+			<Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
 			<SeriesCollectionDirective>
-				{stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+				{stackedCustomSeries.map((item, index) => (
+					<SeriesDirective key={index} {...item} />
+				))}
 			</SeriesCollectionDirective>
+		</ChartComponent>
+	);
+};
 
-			</ChartComponent>
-		)
-}
-
-export default Stacked
+export default Stacked;
